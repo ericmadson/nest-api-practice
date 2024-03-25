4bd6d4d2a1375e8393555a9ff1df3601e79ad030
Eric Madson Almeida <ericmadson67@hotmail.com>
2024-03-25 00:06:24 -0300

    feat: list users

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('users')
 export class UserController {
   @Post()
   async create(@Body() body) {
     return { body };
+  }
+
+  @Get()
+  async list() {
+    return { users: [] };
+  }
+
+  @Get(':id')
+  async show(@Param() params) {
+    return { user: {}, params };
   }
 }
